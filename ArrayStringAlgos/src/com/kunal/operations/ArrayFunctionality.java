1556ff9da7ece2158851b982fc3ab9307072483e
Kumar Kunal <kumarkunal@apples-MacBook-Pro.local>
2019-07-19 18:03:38 +0530

    Array Pivot Implemetation

--- ArrayStringAlgos/src/com/kunal/operations/ArrayFunctionality.java
+++ ArrayStringAlgos/src/com/kunal/operations/ArrayFunctionality.java
@@ -1,5 +1,10 @@
 package com.kunal.operations;
 
+enum TraverseDirection{
+	LEFT,
+	RIGHT	
+}
+
 public class ArrayFunctionality {
 
 	public int pivotIndex(int[] nums) {
@@ -11,30 +16,125 @@ public class ArrayFunctionality {
         for (int i = 0; i <= midLenghtBreak; i++) {
 			differenceTillMidlength = differenceTillMidlength + nums[i]-nums[arrayLenngth-1-i];
 		}
-        if(arrayLenngth%2 != 0) {
-        	
-        }else {
+        if(arrayLenngth%2 != 0 ) {
+        	if(differenceTillMidlength > 0) {
+        		for (int i = midLenghtBreak+1; i > 0; i--) {
+					if(i== midLenghtBreak+1) {
+						differenceTillMidlength = differenceTillMidlength-nums[midLenghtBreak+1];
+						if(differenceTillMidlength == 0) {
+							break;
+						}
+					}else {
+						differenceTillMidlength = differenceTillMidlength-(2*nums[i]);
+						if(differenceTillMidlength == 0) {
+							break;
+						}
+					}
+				}
+        		
+        		if(differenceTillMidlength == 0) {
+        			//pivotIdex = this.checkForLeftMostPresenceOfZero(i-1, 0, TraverseDirection.LEFT, nums);
+        		}else {
+        			pivotIdex = -1;
+        		}
+        		
+        	}else if(differenceTillMidlength < 0) {
+        		for (int i = midLenghtBreak+1; i < nums.length; i++) {
+        			if(i == midLenghtBreak+1) {
+        				differenceTillMidlength = differenceTillMidlength+nums[midLenghtBreak+1];
+        				if(differenceTillMidlength ==0) {
+        					break;  
+        				}
+        			}else {
+        				differenceTillMidlength = differenceTillMidlength+(2*nums[i]);
+        				if(differenceTillMidlength == 0) {
+        					break;
+        				}
+        			}
+					
+				}
+        		
+        		if(differenceTillMidlength == 0) {
+        			//pivotIdex = this.checkForLeftMostPresenceOfZero(startIndex, endIndex, direction, nums);
+        		}
+        		
+        		
+        		
+        	}else if(differenceTillMidlength == 0){
+        		
+        	}
+        }else if(arrayLenngth%2 ==0 && differenceTillMidlength%2==0) {
         	if(differenceTillMidlength > 0) {
         		int i=midLenghtBreak;
         		for(; i > 0 ; i--) {
         			differenceTillMidlength = differenceTillMidlength - (2*nums[i]);
         			if(differenceTillMidlength == 0) {
-        				
         				break;
         			}
         		}
-        		if(nums[i-1]!=0) {
-        			pivotIdex = i-1;
+        		if(differenceTillMidlength == 0) {
+        			//pivotIdex = i-1;
+        			int leftMostZeroIndex = this.checkForLeftMostPresenceOfZero(i-1, 0, TraverseDirection.LEFT, nums);
+        			pivotIdex = leftMostZeroIndex;
+        		}else {
+        			pivotIdex = -1;
         		}
-        	}else if(differenceTillMidlength < 0) {
+        	}else if(differenceTillMidlength < 0 ) {
+        		int i = midLenghtBreak;
+        		for ( i = midLenghtBreak+1; i < nums.length; i++) {
+					differenceTillMidlength = differenceTillMidlength + (2*nums[i]);
+					if(differenceTillMidlength == 0) {
+						break;
+					}
+				}
+        		
+        		if(differenceTillMidlength == 0) {
+        			int leftMostZeroIndex = this.checkForLeftMostPresenceOfZero(i+1, nums.length, TraverseDirection.RIGHT, nums);
+        			pivotIdex = leftMostZeroIndex;
+        		}else {
+        			pivotIdex = -1;
+        		}
         		
         	}else if(differenceTillMidlength == 0) {
-        		
+        		int i=midLenghtBreak;
+        		int leftMostZeroIndex = this.checkForLeftMostPresenceOfZero(i-1, 0, TraverseDirection.LEFT, nums);
+        		pivotIdex = leftMostZeroIndex;
+        		if(pivotIdex != -1) {
+        			pivotIdex = this.checkForLeftMostPresenceOfZero(midLenghtBreak+1, nums.length, TraverseDirection.RIGHT, nums);
+        		}
         	}
         }
-        
-        
         return pivotIdex;
-    }
+    
+	}
+	
+	/* Return index 0 is poresent in particular direction. else return -1*/
+	public int checkForLeftMostPresenceOfZero(int startIndex, int endIndex, TraverseDirection direction, int[] nums) {
+		int returnigZeroIndex = -1;
+		if(direction == TraverseDirection.LEFT) {
+			if(startIndex>= endIndex) {
+				for (int i = startIndex; i >= endIndex; i--) {
+					if(nums[i] == 0) {
+						returnigZeroIndex = i;
+					}
+					if(returnigZeroIndex != -1 && nums[i]!= 0) {
+						break;
+					}
+				}
+			}
+		}else {
+			
+			if(startIndex <= endIndex) {
+				for (int i = startIndex; i < nums.length; i++) {
+					if(nums[i]== 0) {
+						returnigZeroIndex = i;
+						break;
+					}
+				}
+			}
+			
+		}
+		return returnigZeroIndex;
+	}
 
 }
